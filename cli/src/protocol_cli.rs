c4d0bf6956719c61aa921d05aea384ba6904dbdb
equation-man <bigtechguyz@gmail.com>
2026-07-12 01:02:58 +0300

    Fixing solana-instruction account dependency

--- cli/src/protocol_cli.rs
+++ cli/src/protocol_cli.rs
@@ -7,19 +7,20 @@ use solana_sdk::{
     transaction::Transaction,
     instruction::{AccountMeta, Instruction},
     hash::Hash,
+    system_instruction,
 };
 use shellexpand;
 use spl_token;
+use std::str::FromStr;
+use std::fs;
+use toml;
+use serde::Deserialize;
 use solana_client::rpc_client::{RpcClient};
 use solana_commitment_config::CommitmentConfig;
 use spl_associated_token_account::{
-    instruction::create_associated_token_account,
     get_associated_token_address,
 };
 use std::path::Path;
-use megaswap_protocol::instructions::initialize::{
-    InitializeAccounts, InitializeInstructionData,
-};
 
 const USAGE: &str = "
 MEGASWAP TRADING TERMINAL:
@@ -33,6 +34,21 @@ Summary and usage of available commands.
 3. Swaping or Trading.
     megaswap-cli swap --deposit --token <TOKEN TO DEPOSIT> --amount <AMOUNT TO SWAP>
 ";
+
+const SYSTEM_PROGRAM_ID: &str = "11111111111111111111111111111111";
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct Config {
+    pub megaswap_protocol_program_id: String,
+    pub rpc_url: String,
+}
+impl Config {
+    pub fn load() -> anyhow::Result<Self> {
+        let contents = fs::read_to_string("./cli/Config.toml")?;
+
+        Ok(toml::from_str(&contents)?)
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(name="megaswap-cli")]
@@ -94,18 +110,11 @@ pub struct SwapArgs {
     amount: u64,
 }
 
-//#[derive(Debug, Clone, Copy)]
-//pub struct TradeEnvSetup {
-//    mint_x: Pubkey,
-//    mint_y: Pubkey,
-//    lp_mint: Pubkey,
-//}
-
 fn load_or_create_wallet(path: &str) -> anyhow::Result<Keypair> {
     let path_obj = Path::new(path);
 
     if path_obj.exists() {
-        println!("Wallet detected loading...");
+        println!("Playground wallet detected loading...");
         // Load path if it exists
         Ok(
             read_keypair_file(path).map_err(|e| anyhow::anyhow!("{}", e))?
@@ -119,9 +128,9 @@ fn load_or_create_wallet(path: &str) -> anyhow::Result<Keypair> {
 
         // Safe changes to disk.
         let bytes = keypair.to_bytes();
-        println!("Wallet not found creating...");
+        println!("Playground wallet not found creating...");
         std::fs::write(path, serde_json::to_string(&bytes.to_vec())?)?;
-        println!("Wallet created {}", path);
+        println!("Your playground wallets created at {}", path);
 
         Ok(keypair)
     }
@@ -129,22 +138,19 @@ fn load_or_create_wallet(path: &str) -> anyhow::Result<Keypair> {
 
 pub fn ensure_protocol_ready(seed: u64, fee: u16) -> anyhow::Result<()> {
     println!("Ensuring the protocol is ready...");
-    let program_id = Pubkey::from(megaswap_protocol::ID);
-    let mint_x_keypair = load_or_create_wallet("wallets/mint_x.json")?;
-    let mint_y_keypair = load_or_create_wallet("wallets/mint_y.json")?;
+    let config = Config::load()?;
+    let program_id = Pubkey::from_str(&config.megaswap_protocol_program_id)?;
     let (config_pda, config_bump) = Pubkey::find_program_address(
-        &[b"config", &seed.to_le_bytes(), mint_x_keypair.pubkey().as_ref(), mint_y_keypair.pubkey().as_ref()], &program_id,
+        &[b"config"], &program_id,
     );
     let (mint_lp, lp_bump) = Pubkey::find_program_address(
         &[b"lp_mint", config_pda.as_ref()], &program_id
     );
-    let rpc_client = RpcClient::new(
-        "https://api.devnet.solana.com".to_string(),
-    );
+    let rpc_client = RpcClient::new(config.rpc_url);
     let account = rpc_client.get_account_with_commitment(
         &config_pda, CommitmentConfig::confirmed(),
     ).map_err(|e| anyhow::anyhow!("{}", e))?;
-    let recent_blockhash: Hash = rpc_client.get_latest_blockhash()?;
+    println!("Solana rpc result is {:?}", &account);
 
     match account.value {
         Some(account) => {
@@ -153,9 +159,12 @@ pub fn ensure_protocol_ready(seed: u64, fee: u16) -> anyhow::Result<()> {
             println!("The config account is {:?}", account);
         }
         None => {
+            println!("Protocol no yet initialized! Initializing...");
             let initializer = read_keypair_file(
                 shellexpand::tilde("~/.config/solana/id.json").to_string()
             ).map_err(|e| anyhow::anyhow!("{}", e))?;
+            let mint_x_keypair = load_or_create_wallet("wallets/mint_x.json")?;
+            let mint_y_keypair = load_or_create_wallet("wallets/mint_y.json")?;
 
             // Computing the deterministic ATA. Token vaults held by PDAs
             let vault_x_ata = get_associated_token_address(
@@ -164,29 +173,36 @@ pub fn ensure_protocol_ready(seed: u64, fee: u16) -> anyhow::Result<()> {
             let vault_y_ata = get_associated_token_address(
                 &config_pda, &mint_y_keypair.pubkey()
             );
-            let initialize_data = InitializeInstructionData {
-                seed, fee, mint_x: mint_x_keypair.pubkey().to_bytes(),
-                mint_y: mint_y_keypair.pubkey().to_bytes(),
-                config_bump: [config_bump],
-                lp_mint_decimals: 6,
-                lp_bump: [lp_bump],
-                authority: initializer.pubkey().to_bytes()
-            };
+            let mut ix_data = vec![0u8];
+            ix_data.extend_from_slice(&seed.to_le_bytes());
+            ix_data.extend_from_slice(&fee.to_le_bytes());
+            ix_data.extend_from_slice(&mint_x_keypair.pubkey().to_bytes());
+            ix_data.extend_from_slice(&mint_y_keypair.pubkey().to_bytes());
+            ix_data.push(config_bump);
+            ix_data.push(6u8);
+            ix_data.push(lp_bump);
+            ix_data.extend_from_slice(&initializer.pubkey().to_bytes());
+
             let accounts = vec![
                 AccountMeta::new(initializer.pubkey(), true),
-                AccountMeta::new(config_pda, false),
                 AccountMeta::new(vault_x_ata, false),
                 AccountMeta::new(vault_y_ata, false),
                 AccountMeta::new_readonly(mint_x_keypair.pubkey(), false),
                 AccountMeta::new_readonly(mint_y_keypair.pubkey(), false),
                 AccountMeta::new(mint_lp, false),
+                AccountMeta::new(config_pda, false),
                 AccountMeta::new_readonly(spl_associated_token_account::id(), false),
-                AccountMeta::new_readonly(system_program::id(), false),
+                AccountMeta::new_readonly(Pubkey::from_str(SYSTEM_PROGRAM_ID)?, false),
                 AccountMeta::new_readonly(spl_token::id(), false),
             ];
-            let init_ix = Instruction { program_id, accounts, instruction_data };
+            let init_ix = Instruction {
+                program_id: program_id,
+                accounts: accounts,
+                data: ix_data
+            };
+            let recent_blockhash: Hash = rpc_client.get_latest_blockhash()?;
             let init_tx = Transaction::new_signed_with_payer(
-                &[init_ix], Some(&initializer.pubkey()), &[initializer]], recent_blockhash
+                &[init_ix], Some(&initializer.pubkey()), &[&initializer], recent_blockhash
             );
             rpc_client.send_and_confirm_transaction(&init_tx)?;
             println!("Protocol initialized successfully...");
@@ -202,8 +218,9 @@ pub fn process_cli() {
         Commands::Exec(args) => {
             match args.init.as_str() {
                 "initializer" => {
-                    println!("Calling cli initialize command arg is, {}", args.init);
-                    let _ = ensure_protocol_ready(42, 200);
+                    //println!("Calling cli initialize command arg is, {}", args.init);
+                    let proto_ready_res = ensure_protocol_ready(42, 200);
+                    println!("Protocol readiness {:?}", proto_ready_res);
                     //initialize_proto();
                 },
                 _ => eprintln!("Error Initializing the protocol")
